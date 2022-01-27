27aaf3dbc5087738d6e074c62ccd5023e0397b67
PatrickGABrown <patrick.brown@revature.net>
2022-01-26 20:00:16 -0500

    New changes to project0

---
+++ src/main/scala/revature/Rando.scala
@@ -0,0 +1,11 @@
+package revature
+
+import java.time._
+
+object Rando {
+  def main(args: Array[String]): Unit ={
+    var x = LocalDate.now.getYear
+    println("The current year is " + x)
+  }
+
+}
